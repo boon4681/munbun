b3037e874de8c0df42872b5b7c45d7c1557baf27
boon4681 <boon4681dev@gmail.com>
2025-01-01 09:15:15 +0700

    feat: more appropriate login

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -5,7 +5,7 @@
 	import { ModeWatcher } from "mode-watcher";
 	import { onMount } from "svelte";
 	import { redirect } from "@sveltejs/kit";
-    import MunbunLogin from "@/components/munbun/auth/munbun-login.svelte";
+	import MunbunLogin from "@/components/munbun/auth/munbun-login.svelte";
 	onMount(() => {
 		if (data.login) {
 			redirect(300, "/");
@@ -16,21 +16,15 @@
 {#if data.login}
 	<slot></slot>
 {:else if !data.setup}
-	<div class="lg:p-8">
-		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-			<div class="flex flex-col space-y-2 text-center">
-				<h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
-			</div>
-			<MunbunLogin></MunbunLogin>
-			<!-- <p class="text-muted-foreground px-8 text-center text-sm">
-				By clicking continue, you agree to our
-				<a href="/terms" class="hover:text-primary underline underline-offset-4"> Terms of Service </a>
-				and
-				<a href="/privacy" class="hover:text-primary underline underline-offset-4"> Privacy Policy </a>
-				.
-			</p> -->
-		</div>
+	<div class="lg:p-8 w-full max-w-screen-sm mx-auto border mt-[200px]">
+		<h1 class="text-2xl font-semibold tracking-tight">Munbun</h1>
+		<div class="mb-4 text-muted-foreground">Create Superuser account</div>
+		<MunbunLogin></MunbunLogin>
 	</div>
 {:else}
-	<MunbunLogin></MunbunLogin>
+	<div class="lg:p-8 w-full max-w-screen-sm mx-auto border mt-[200px]">
+		<h1 class="text-2xl font-semibold tracking-tight">Munbun</h1>
+		<div class="mb-4 text-muted-foreground">Login</div>
+		<MunbunLogin></MunbunLogin>
+	</div>
 {/if}

85a03e3c82d35c87637b023f6e2cf7fd63c5fd1d
boon4681 <boon4681dev@gmail.com>
2025-01-01 09:48:52 +0700

    fixed: build failed again

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,5 @@
 import { drizzle } from 'drizzle-orm/better-sqlite3';
 import Database from 'better-sqlite3';
-import { env } from '$env/dynamic/private';
 import * as schema from "$server/db/schema"
 import { eq } from 'drizzle-orm';
 import fs from 'node:fs';
